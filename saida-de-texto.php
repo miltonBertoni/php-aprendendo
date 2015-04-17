866d3039ef609d302fe829fd48858524cf8e30c6
Milton Bertoni <miltonnunes2012@gmail.com>
2015-04-17 17:09:51 -0300

    Aprendendo sobre os tipos de saida de texto

---
+++ saida-de-texto.php
@@ -0,0 +1,20 @@
+<?php
+header ('Content-type: text/html; charset=UTF-8');
+
+//$var = echo 'Olá mundo!';
+
+$var = print 'Olá mundo!';
+$numero = 10;
+$operador = '!!';
+
+echo '<br>Olá mundo! ' . ' ' . $numero . $operador;
+
+echo '<br>';
+
+echo "<br>Olá mundo! " . " " . $numero . $operador;
+echo '<br>';
+
+echo "<br>Olá mundo!$numero$operador";
+echo '<br>';
+
+echo '<br>Olá mundo!$numero$operador';
